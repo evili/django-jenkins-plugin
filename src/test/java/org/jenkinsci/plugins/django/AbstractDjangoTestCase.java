38798de681c202ad179fbe1f45a80ecc908c0a4b
Evili del Rio i Silvan <erio@iri.upc.edu>
2015-01-30 12:29:19 +0100

    First test

--- src/test/java/org/jenkinsci/plugins/django/AbstractDjangoTestCase.java
+++ src/test/java/org/jenkinsci/plugins/django/AbstractDjangoTestCase.java
@@ -1,12 +1,24 @@
 package org.jenkinsci.plugins.django;
 
-public abstract class AbstractDjangoTestCase extends HudsonTestCase {
+import hudson.model.FreeStyleBuild;
+import hudson.model.FreeStyleProject;
+import hudson.tasks.Shell;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.apache.commons.io.FileUtils;
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
 
-
-    protected FreeStyleProject setupProject()
-        throws Exception {
-        FreeStyleProject project = createFreeStyleProject();
-        return project;
-    }
-    
+public abstract class AbstractDjangoTestCase {
+	
+	@Rule public JenkinsRule j = new JenkinsRule();
+	@Test public void first() throws Exception {
+		FreeStyleProject project = j.createFreeStyleProject();
+		project.getBuildersList().add(new Shell("echo hello"));
+		FreeStyleBuild build = project.scheduleBuild2(0).get();
+		System.out.println(build.getDisplayName() + " completed");
+		String s = FileUtils.readFileToString(build.getLogFile());
+		assertThat(s, containsString("Help"));
+	}
 };
---
+++ src/test/java/org/jenkinsci/plugins/django/DjangoBuilderTestCase.java
@@ -0,0 +1,4 @@
+package org.jenkinsci.plugins.django;
+
+public class DjangoBuilderTestCase extends AbstractDjangoTestCase {
+}
