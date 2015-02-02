cb204e48636f18b3dc209c9a42de9841a1e57568
Evili del Rio i Silvan <evili.del.rio@gmail.com>
2015-02-02 09:20:51 +0100

    Test setup

--- src/test/java/org/jenkinsci/plugins/django/AbstractDjangoTestCase.java
+++ src/test/java/org/jenkinsci/plugins/django/AbstractDjangoTestCase.java
@@ -1,24 +1,20 @@
 package org.jenkinsci.plugins.django;
 
-import hudson.model.FreeStyleBuild;
+import junit.framework.TestCase;
 import hudson.model.FreeStyleProject;
-import hudson.tasks.Shell;
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
-import org.apache.commons.io.FileUtils;
+
 import org.junit.Rule;
-import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
 
-public abstract class AbstractDjangoTestCase {
+
+public abstract class AbstractDjangoTestCase extends TestCase {
+
+	protected FreeStyleProject project;
+	protected DjangoJenkinsBuilder djangoBuilder;
 	
-	@Rule public JenkinsRule j = new JenkinsRule();
-	@Test public void first() throws Exception {
-		FreeStyleProject project = j.createFreeStyleProject();
-		project.getBuildersList().add(new Shell("echo hello"));
-		FreeStyleBuild build = project.scheduleBuild2(0).get();
-		System.out.println(build.getDisplayName() + " completed");
-		String s = FileUtils.readFileToString(build.getLogFile());
-		assertThat(s, containsString("Help"));
+	@Rule public JenkinsRule jenkinsRule = new JenkinsRule();
+	@Override
+	protected void setUp() throws Exception {
+		project = jenkinsRule.createFreeStyleProject("testdjango");
 	}
-};
+}
