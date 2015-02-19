5f4c589b3e0e1d4c5d474bcae86a88897628a5df
Evili del Rio i Silvan <evili.del.rio@gmail.com>
2015-02-19 17:24:27 +0100

    Scan only the last line of output

--- src/test/java/org/jenkinsci/plugins/django/ITDjangoJenkinsBuilder.java
+++ src/test/java/org/jenkinsci/plugins/django/ITDjangoJenkinsBuilder.java
@@ -10,6 +10,7 @@ import java.util.EnumSet;
 import jenkins.scm.DefaultSCMCheckoutStrategyImpl;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.StringUtils;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
@@ -32,6 +33,8 @@ public class ITDjangoJenkinsBuilder {
 		project.setScmCheckoutStrategy(scmCheckoutStrategy);
 		FreeStyleBuild build = project.scheduleBuild2(1).get();
 		String s = FileUtils.readFileToString(build.getLogFile());
-		assertTrue("Test Project Build should be successful.", s.contains("Finished: SUCCESS"));
+		String[] lines = StringUtils.split(s, "\n\r");
+		String lastL = lines[lines.length-1];
+		assertTrue("Test Project Build should be successful:"+lastL, lastL.contains("Finished: SUCCESS"));
 	}
 }
